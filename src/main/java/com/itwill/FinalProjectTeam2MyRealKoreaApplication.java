2a7bfa4aa342a7f970733d3c1d7e4dfcba1ba4a7
minseonallykim <minsun947@gmail.com>
2023-03-24 19:04:34 +0900

    .

---
+++ src/main/java/com/itwill/FinalProjectTeam2MyRealKoreaApplication.java
@@ -0,0 +1,13 @@
+package com.itwill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectTeam2MyRealKoreaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectTeam2MyRealKoreaApplication.class, args);
+	}
+
+}

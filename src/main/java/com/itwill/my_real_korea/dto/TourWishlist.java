28611dacfb6a14f0da0e8a3118183f0f9a395343
jeong-yoona <jeong-yoona@localhost>
2023-03-26 17:11:16 +0900

    ..

---
+++ src/main/java/com/itwill/my_real_korea/dto/TourWishlist.java
@@ -0,0 +1,25 @@
+package com.itwill.my_real_korea.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/*
+이름         널?       유형           
+---------- -------- ------------ 
+TO_WISH_NO NOT NULL NUMBER       
+TO_NO               NUMBER       
+USER_ID             VARCHAR2(50) 
+WISH_NO             NUMBER  
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TourWishlist {
+	//PK
+	private int toWishNo;
+	//FK
+	private Tour tour;
+	private UserInfo userinfo;
+	private Wishlist wishNo;
+}
---
+++ src/main/java/com/itwill/my_real_korea/dto/UserInfo.java
@@ -0,0 +1,5 @@
+package com.itwill.my_real_korea.dto;
+
+public class UserInfo {
+	private String userId;
+}

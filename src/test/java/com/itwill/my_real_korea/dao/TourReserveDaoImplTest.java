dd427feb9b4f130588b926eaf04ca762c63a7745
jeong-yoona <jeong-yoona@192.168.25.15>
2023-03-27 02:03:54 +0900

    ..

---
+++ src/test/java/com/itwill/my_real_korea/dao/TourReserveDaoImplTest.java
@@ -0,0 +1,44 @@
+package com.itwill.my_real_korea.dao;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TourReserveDaoImplTest {
+
+	@Test
+	void testTourReserveDaoImpl() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testInsertTourReserve() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUpdateTourReserve() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeleteTourReserve() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSelectByToRsNo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSelectAllTourReserveByUserId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeleteAllTourReserveByUserId() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/com/itwill/my_real_korea/dao/TourWishlistDaoImplTest.java
@@ -0,0 +1,34 @@
+package com.itwill.my_real_korea.dao;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TourWishlistDaoImplTest {
+
+	@Test
+	void testTourWishlistDaoImpl() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testInsertTourWishlist() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeleteTourWishlist() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSelectAllWishlistByUserId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSelectWishlistByToWishNo() {
+		fail("Not yet implemented");
+	}
+
+}

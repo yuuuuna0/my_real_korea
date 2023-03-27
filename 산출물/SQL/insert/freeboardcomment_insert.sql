fae13167b41ce85b5416fa885ac4195aee9cfef7
김민영 <117143516+ssssoooop@users.noreply.github.com>
2023-03-27 22:52:08 +0900

    Create freeboardcomment_insert.sql

---
+++ 산출물/SQL/insert/freeboardcomment_insert.sql
@@ -0,0 +1,4 @@
+--insert
+insert into free_board values (free_board_f_bo_no_seq.nextval,'명동교자 칼국수 맛있어요',sysdate,0,1,'user3');
+insert into free_board values (free_board_f_bo_no_seq.nextval,'카페는 어떠신가요.',sysdate,0,2,'user1');
+insert into free_board values (free_board_f_bo_no_seq.nextval,'강원 맛집 저도 궁금하군요.',sysdate,0,3,'user2');

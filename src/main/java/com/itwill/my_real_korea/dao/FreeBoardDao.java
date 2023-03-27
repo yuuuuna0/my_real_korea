e759cff46ca657804b5b223ca797139e7be9f949
김민영 <117143516+ssssoooop@users.noreply.github.com>
2023-03-27 10:45:10 +0900

    Update FreeBoardDao.java

--- src/main/java/com/itwill/my_real_korea/dao/FreeBoardDao.java
+++ src/main/java/com/itwill/my_real_korea/dao/FreeBoardDao.java
@@ -1,10 +1,9 @@
-package com.itwill.my_real_korea.Dao;
+package com.itwill.my_real_korea.dao;
 
 import java.util.List;
-import java.util.Map;
 
-import com.itwill.my_real_korea.service.FreeBoard;
-import com.itwill.my_real_korea.service.FreeBoardListPageMakerDto;
+import com.itwill.my_real_korea.dto.FreeBoard;
+import com.itwill.my_real_korea.dto.FreeBoardListPageMakerDto;
 
 public interface FreeBoardDao {
 
@@ -13,9 +12,9 @@ public interface FreeBoardDao {
  */
 int insertContent(FreeBoard freeBoard) throws Exception;
 /*
- * 자유게시판 게시글,댓글 번호(boardno)로 해당 게시글 보기 
+ * 자유게시판 게시글번호(boardno)로 해당 게시글 보기 
  */
- selectByNo(int fBoNo) throws Exception;
+FreeBoard selectByNo(int fBoNo) throws Exception;
 /*
  * 자유게시판 리스트 보기 (게시글 시작번호, 게시글 끝번호) - 페이징 처리
  */
@@ -24,16 +23,16 @@ List<FreeBoard> selectAll(int pageStart, int pageEnd) throws Exception;
 FreeBoardListPageMakerDto selectAll(int currentPage) throws Exception;
 
 /*
- * 게시글,댓글 title 출력 설정
+ * 게시글 title 출력 설정
  */
 String getTitleString(FreeBoard freeBoard) throws Exception;
 
 /*
- *  게시글,댓글 삭제
+ *  게시글 삭제
  */
 int deleteContent(int fBoNo) throws Exception;
 /*
- *  게시글,댓글 내용 수정
+ *  게시글 내용 수정
  */
 int updateContent(FreeBoard freeBoard) throws Exception;
 /*
@@ -41,7 +40,7 @@ int updateContent(FreeBoard freeBoard) throws Exception;
  */
 int increaseContentReadCount(int fBoNo) throws Exception;
 /*
- * 공지사항 게시글,댓글 총 개수 조회
+ * 공지사항 게시글 총 개수 조회
  */
 int selectContentCount() throws Exception;
 

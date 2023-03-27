07e22c9d7fafb1f965f2fa39473384b862543548
sdongpil <pildong94@naver.com>
2023-03-27 16:35:11 +0900

    freeBoard_insert.sql 추가

---
+++ 산출물/SQL/insert/freeBorad_insert.sql
@@ -0,0 +1,4 @@
+--insert
+insert into free_board values (free_board_f_bo_no_seq.nextval,'서울 맛집 추천','추천받습니다',sysdate,0,1,'user1');
+insert into free_board values (free_board_f_bo_no_seq.nextval,'경기 맛집 추천','추천받습니다',sysdate,0,2,'user2');
+insert into free_board values (free_board_f_bo_no_seq.nextval,'강원 맛집 추천','추천받습니다',sysdate,0,3,'user3');

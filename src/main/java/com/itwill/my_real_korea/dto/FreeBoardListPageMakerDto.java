2dbf35600b36d9eb1d1dcb0f56f25e5b73ec16d0
김민영 <117143516+ssssoooop@users.noreply.github.com>
2023-03-27 09:25:19 +0900

    Update FreeBoardListPageMakerDto.java

--- src/main/java/com/itwill/my_real_korea/dto/FreeBoardListPageMakerDto.java
+++ src/main/java/com/itwill/my_real_korea/dto/FreeBoardListPageMakerDto.java
@@ -2,8 +2,7 @@ package com.itwill.my_real_korea.dto;
 
 import java.util.List;
 
-public class FreeBoardPagMaker {
+public class FreeBoardPageMaker {
 	public List<FreeBoard> FreeBoardList;
-	public List<FreeBoardComment> FreeBoardCommentList;
 	public PageMaker pageMaker;
 }

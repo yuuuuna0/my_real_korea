70184acd3c78d387bdfc2531f1b111bb282c8331
김민영 <117143516+ssssoooop@users.noreply.github.com>
2023-03-27 11:15:51 +0900

    Update FreeBoardCommentDao.java

--- src/main/java/com/itwill/my_real_korea/dao/FreeBoardCommentDao.java
+++ src/main/java/com/itwill/my_real_korea/dao/FreeBoardCommentDao.java
@@ -1,17 +1,6 @@
-package com.itwill.my_real_korea.mapper;
-
-import java.util.List;
-import java.util.Map;
-
-import com.itwill.my_real_korea.dto.FreeBoardComment;
-import com.itwill.my_real_korea.service.FreeBoardCommentListPageMakerDto;
-
-public interface FreeBoardCommentDao {
-
 package com.itwill.my_real_korea.dao;
 
 import java.util.List;
-import java.util.Map;
 
 import com.itwill.my_real_korea.dto.FreeBoardComment;
 import com.itwill.my_real_korea.dto.FreeBoardCommentListPageMakerDto;
@@ -19,14 +8,14 @@ import com.itwill.my_real_korea.dto.FreeBoardCommentListPageMakerDto;
 public interface FreeBoardCommentDao {
 
 	/*
-	 * 자유게시판 게시글 추가
+	 * 자유게시판 답변 추가
 	 */
-	int insertContent(FreeBoardComment freeBoardComment) throws Exception;
+	int insertComment(FreeBoardComment freeBoardComment) throws Exception;
 	/*
 	 * 답변 번호(boardno)로 해당 답변 보기 
 	 */
 	FreeBoardComment selectByNo(int fCommentNo) throws Exception;
-  /*
+  	/*
 	 * 게시물 번호(boardno)로 해당 답변 보기 
 	 */
 	FreeBoardComment selectByContentNo(int fBoNo) throws Exception;

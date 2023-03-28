95f4b986d688119eaf8926ca2543b60f043a9dd5
minseonallykim <minsun947@gmail.com>
2023-03-28 14:52:31 +0900

    fff

--- 산출물/SQL/insert/notice_insert.sql
+++ 산출물/SQL/insert/notice_insert.sql
@@ -7,8 +7,8 @@ insert into user_info(user_id,password,name,nickname,phone,email,birth,address,g
 insert into user_info(user_id,password,name,nickname,phone,email,birth,address,gender) values('kms3','3333','김3','별명3','010-3333-3333','kms3@korea.com','1993-03-03','강남구',0);
 
 -- 공지사항 notice insert
-insert into notice(n_no,n_title,n_content,n_img,user_id) values (notice_n_no_seq.nextval,'공지사항1','공지사항내용1','img.png','kms1'); 
-insert into notice(n_no,n_title,n_content,n_img,user_id) values (notice_n_no_seq.nextval,'공지사항2','공지사항내용2','img.png','kms2'); 
-insert into notice(n_no,n_title,n_content,n_img,user_id) values (notice_n_no_seq.nextval,'공지사항3','공지사항내용3','img.png','kms3'); 
+insert into notice(n_no,n_title,n_content,n_date,n_readcount,n_img,user_id) values (notice_n_no_seq.nextval,'공지사항1','공지사항내용1',sysdate,0,'img.png','kms1'); 
+insert into notice(n_no,n_title,n_content,n_date,n_readcount,n_img,user_id) values (notice_n_no_seq.nextval,'공지사항2','공지사항내용2',sysdate,0,'img.png','kms2'); 
+insert into notice(n_no,n_title,n_content,n_date,n_readcount,n_img,user_id) values (notice_n_no_seq.nextval,'공지사항3','공지사항내용3',sysdate,0,'img.png','kms3'); 
 
 

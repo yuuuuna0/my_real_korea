dcaaead962bbdad5bb7d080bcb7c7068e2a61c64
jeong-yoona <jeong-yoona@localhost>
2023-03-29 17:18:55 +0900

    tour dto 수

--- src/main/java/com/itwill/my_real_korea/dto/tour/Tour.java
+++ src/main/java/com/itwill/my_real_korea/dto/tour/Tour.java
@@ -40,5 +40,25 @@ public class Tour {
 	private String toNotice;
 	private int toCount;
 	private City city;		//FK
+	private List<TourImg> tourImgList;
+	
+	public Tour(int toNo, String toName, int toType, int toTime, int toPerson, String toMeet, int toPrice,
+			String toInfo, String toNotice, int toCount, City city) {
+		super();
+		this.toNo = toNo;
+		this.toName = toName;
+		this.toType = toType;
+		this.toTime = toTime;
+		this.toPerson = toPerson;
+		this.toMeet = toMeet;
+		this.toPrice = toPrice;
+		this.toInfo = toInfo;
+		this.toNotice = toNotice;
+		this.toCount = toCount;
+		this.city = city;
+		this.tourImgList=new ArrayList();
+	}
+	
+	
 	
 }

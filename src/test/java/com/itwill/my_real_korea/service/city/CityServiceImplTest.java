b27bd3eec18325adcaf3c43583cb99a3e6219932
jeong-yoona <jeong-yoona@localhost>
2023-03-31 14:08:40 +0900

    ..

--- src/test/java/com/itwill/my_real_korea/service/city/CityServiceImplTest.java
+++ src/test/java/com/itwill/my_real_korea/service/city/CityServiceImplTest.java
@@ -8,12 +8,13 @@ import org.junit.jupiter.api.Test;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
 
 import com.itwill.my_real_korea.dao.city.CityDao;
 import com.itwill.my_real_korea.dto.City;
 import com.itwill.my_real_korea.mapper.CityMapper;
 @SpringBootTest
-@MapperScan(basePackageClasses = CityMapper.class)
+@ComponentScan(basePackages = {"com.itwill.my_real_korea"})
 class CityServiceImplTest {
 	@Autowired
 	private CityService cityService;
@@ -24,12 +25,12 @@ class CityServiceImplTest {
 		assertEquals(rowCount, 1);
 	}
 
+	@Test
 	void testFindByCityNo() throws Exception{
 		System.out.println("3번 도시:" +cityService.findByCityNo(3));
 		System.out.println("6번 도시:" +cityService.findByCityNo(6));
 	}
 
-	@Test
 	void testSelectAllCity() throws Exception{
 		List<City> cityList=cityService.findAllCity();
 		for (City city : cityList) {

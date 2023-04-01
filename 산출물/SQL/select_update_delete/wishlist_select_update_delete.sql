1d80ad8ad6b6b2cb3a524f627c95fc237b9a90e4
minseonallykim <minsun947@gmail.com>
2023-04-02 01:00:55 +0900

    wishlist sql 수정

--- 산출물/SQL/select_update_delete/wishlist_select_update_delete.sql
+++ 산출물/SQL/select_update_delete/wishlist_select_update_delete.sql
@@ -10,10 +10,10 @@ left join ticket ti on w.ti_no=ti.ti_no
 left join tour tu on w.to_no=tu.to_no
 where user_id='user1';
 
-/*
+
 -- 위시리스트 한 개 보기 (선택)
-select * from wishlist where wish_no=3 and user_id='user2';
-*/
+select * from wishlist where wish_no=3;
+
 
 -- 위시리스트 담겨있는 티켓/투어 상품의 수 
 select count(*) from wishlist where user_id='user1';

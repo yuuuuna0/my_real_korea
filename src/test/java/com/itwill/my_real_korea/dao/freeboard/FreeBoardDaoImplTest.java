a0ba96a26fd71e6b8c68b803acf57637aa512ea8
김민영 <117143516+ssssoooop@users.noreply.github.com>
2023-04-02 12:59:16 +0900

    Update FreeBoardDaoImplTest.java

--- src/test/java/com/itwill/my_real_korea/dao/freeboard/FreeBoardDaoImplTest.java
+++ src/test/java/com/itwill/my_real_korea/dao/freeboard/FreeBoardDaoImplTest.java
@@ -13,46 +13,46 @@ import com.itwill.my_real_korea.dto.freeboard.FreeBoard;
 
 @SpringBootTest
 public class FreeBoardDaoImplTest {
-//	@Autowired
-//	private FreeBoardDao freeBoardDao;
-//	
-//	@Disabled
-//	@Test
-//	void testInsertBoard() {
-//		int rowCount = freeBoardDao.insertBoard(new FreeBoard(0,"자유1","자유내용1",sysdate,0,1,"user1"));
-//		assertEquals(rowCount, 1);
-//	}
-//	
-//	@Disabled
-//	@Test
-//	void testSelectAll() {
-//		fail("Not yet implemented");
-//	}
-//	
-//	@Disabled
-//	void testDeleteBoard() {
-//		fail("Not yet implemented");
-//	}
-//	
-//	@Disabled
-//	void testUpdateBoard() {
-//		fail("Not yet implemented");
-//	}
-//	@Disabled
-//	void testIncreaseReadCount() {
-//		fail("Not yet implemented");
-//	}
-//	@Disabled
-//	void testSelectFreeBoardCount() {
-//		fail("Not yet implemented");
-//	}
-//	@Disabled
-//	void testSelectSearch() {
-//		fail("Not yet implemented");
-//	}
-//	@Disabled
-//	void testSelectSearchList() {
-//		fail("Not yet implemented");
-//	}
+	@Autowired
+	private FreeBoardDao freeBoardDao;
+	
+	@Disabled
+	@Test
+	void testInsertBoard() {
+		int rowCount = freeBoardDao.insertBoard(new FreeBoard(0,"자유1","자유내용1",sysdate,0,1,"user1"));
+		assertEquals(rowCount, 1);
+	}
+	
+	@Disabled
+	@Test
+	void testSelectAll() {
+		fail("Not yet implemented");
+	}
+	
+	@Disabled
+	void testDeleteBoard() {
+		fail("Not yet implemented");
+	}
+	
+	@Disabled
+	void testUpdateBoard() {
+		fail("Not yet implemented");
+	}
+	@Disabled
+	void testIncreaseReadCount() {
+		fail("Not yet implemented");
+	}
+	@Disabled
+	void testSelectFreeBoardCount() {
+		fail("Not yet implemented");
+	}
+	@Disabled
+	void testSelectSearch() {
+		fail("Not yet implemented");
+	}
+	@Disabled
+	void testSelectSearchList() {
+		fail("Not yet implemented");
+	}
 	
 }

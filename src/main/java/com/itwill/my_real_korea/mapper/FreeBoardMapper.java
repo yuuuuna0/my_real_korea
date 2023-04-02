54a26ff934c276c802c734e95e56233e01403ee8
김민영 <117143516+ssssoooop@users.noreply.github.com>
2023-04-02 13:25:04 +0900

    Update FreeBoardMapper.java

--- src/main/java/com/itwill/my_real_korea/mapper/FreeBoardMapper.java
+++ src/main/java/com/itwill/my_real_korea/mapper/FreeBoardMapper.java
@@ -7,7 +7,7 @@ import java.util.Map;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.itwill.my_real_korea.dto.freeboard.FreeBoard;
-import com.itwill.my_real_korea.dto.freeboard.FreeBoardCommentListPageMakerDto;
+import com.itwill.my_real_korea.dto.util.PageMakerDto;
 
 
 @Mapper

229fc8651b88248f1fc2041334d257e4d3b534f2
kgee2 <kgee12300@gmail.com>
2023-04-03 15:23:22 +0900

    user_info 관련 final_insert.sql 수정

--- 산출물/SQL/insert/final_insert.sql
+++ 산출물/SQL/insert/final_insert.sql
@@ -1,15 +1,15 @@
 /*************** user ***********************/
 --user_info
-insert into user_info(user_id,password,name,nickname,phone,email,birth,address,gender,point,is_admin) 
+insert into user_info(user_id,password,name,nickname,phone,email,birth,address,gender,point,is_admin,mail_auth,mail_key) 
 	values('admin','admin0000','관리자','관리자','010-0000-0000','admin0@gmail.com','2000-01-01','테헤란로0',0,0,0,1,0); 
 
-insert into user_info(user_id,password,name,nickname,phone,email,birth,address,gender,point,is_admin) 
+insert into user_info(user_id,password,name,nickname,phone,email,birth,address,gender,point,is_admin,mail_auth,mail_key) 
 	values('user1','user1111','회원1','마리코1','010-1111-1111','user1@gmail.com','2001-01-01','테헤란로1',0,0,1,1,0); 
 
-insert into user_info(user_id,password,name,nickname,phone,email,birth,address,gender,point,is_admin) 
+insert into user_info(user_id,password,name,nickname,phone,email,birth,address,gender,point,is_admin,mail_auth,mail_key) 
 	values('user2','user2222','회원2','마리코2','010-2222-2222','user2@gmail.com','2002-02-02','테헤란로2',1,0,1,1,0); 
 
-insert into user_info(user_id,password,name,nickname,phone,email,birth,address,gender,point,is_admin) 
+insert into user_info(user_id,password,name,nickname,phone,email,birth,address,gender,point,is_admin,mail_auth,mail_key) 
 	values('user3','user3333','회원3','마리코3','010-3333-3333','user3@gmail.com','2003-03-03','테헤란로3',1,0,1,1,0); 
 --user_photo
 insert into user_img(user_img_no,user_img_url,user_id) values(10,'010.png','admin');

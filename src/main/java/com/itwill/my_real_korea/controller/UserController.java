5c0de795daa75f35d695e5adcc2d77f681e0dc85
kgee2 <kgee12300@gmail.com>
2023-04-05 23:25:32 +0900

    user 템플릿, controller 매핑 수정

--- src/main/java/com/itwill/my_real_korea/controller/UserController.java
+++ src/main/java/com/itwill/my_real_korea/controller/UserController.java
@@ -28,62 +28,62 @@ public class UserController {
 	private UserService userService;
 	
 	@ApiOperation(value = "메인페이지")
-	@RequestMapping(value = "/user_main", produces = "application/json;charset=UTF-8")
+	@RequestMapping(value = "/user-main", produces = "application/json;charset=UTF-8")
 	public String user_main() {
-		return "user_main";
+		return "user-main";
 	}
 	
 	@ApiOperation(value = "회원가입 폼")
-	@GetMapping(value = "/user_write_form", produces = "application/json;charset=UTF-8")
+	@GetMapping(value = "/user-write-form", produces = "application/json;charset=UTF-8")
 	public String user_write_form() {
-		String forward_path = "user_write_form";
+		String forward_path = "user-write-form";
 		return forward_path;
 	}
 	
 	@ApiOperation(value = "회원가입 액션")
-	@PostMapping(value = "user_write_action", produces = "application/json;charset=UTF-8")
+	@PostMapping(value = "user-write-action", produces = "application/json;charset=UTF-8")
 	public String user_write_action(@ModelAttribute("fuser") User user,Model model) throws Exception {
 		String forward_path = "";
 		try {
 			userService.create(user);
 			userService.mailKeyUpdate(user);
 //			forward_path="redirect:user_login_form";
-			forward_path="user_main";
+			forward_path="user-main";
 		}catch (ExistedUserException e) {
 			model.addAttribute("msg", e.getMessage());
-			forward_path="user_write_form";
+			forward_path="user-write-form";
 		}
 		return forward_path;
 	}
 	
 	@ApiOperation(value = "로그인 폼")
-	@GetMapping(value = "/user_login_form", produces = "application/json;charset=UTF-8")
+	@GetMapping(value = "/user-login-form", produces = "application/json;charset=UTF-8")
 	public String user_login_form() {
-		String forward_path = "user_login_form";
+		String forward_path = "user-login-form";
 		return forward_path;
 	}
 	
 	@ApiOperation(value = "로그인 액션")
-	@PostMapping(value = "user_login_action", produces = "application/json;charset=UTF-8")
+	@PostMapping(value = "user-login-action", produces = "application/json;charset=UTF-8")
 	public String user_login_action(@ModelAttribute("fuser") User user, Model model, HttpSession session) throws Exception {
 	    String forwardPath = "";
 	    try {
 	        User loginUser = userService.login(user.getUserId(), user.getPassword());
 	        if (loginUser.getMailAuth() == 0) {
 	        	model.addAttribute("authUser", loginUser);
-	            forwardPath = "user_auth_form";
+	            forwardPath = "user-auth-form";
 	        } else {
 	            session.setAttribute("sUserId", loginUser.getUserId());
-	            forwardPath = "redirect:user_main";
+	            forwardPath = "redirect:user-main";
 	        }
 	    } catch (UserNotFoundException e) {
 	        e.printStackTrace();
 	        model.addAttribute("msg1", e.getMessage());
-	        forwardPath = "user_login_form";
+	        forwardPath = "user-login-form";
 	    } catch (PasswordMismatchException e) {
 	        e.printStackTrace();
 	        model.addAttribute("msg2", e.getMessage());
-	        forwardPath = "user_login_form";
+	        forwardPath = "user-login-form";
 	    }
 	    return forwardPath;
 	}
@@ -91,27 +91,27 @@ public class UserController {
 	/************** 수정중 ****************/
 	@LoginCheck
 	@ApiOperation(value = "메일 인증 폼")
-	@PostMapping(value = "/user_auth_form")
+	@PostMapping(value = "/user-auth-form")
 	public String user_auth_form(HttpServletRequest request) throws Exception {
 		String forward_path = "";
 		String sUserId=(String)request.getSession().getAttribute("sUserId");
 		User loginUser = userService.findUser(sUserId);
 		request.setAttribute("loginUser", loginUser);
-		forward_path = "user_auth_form";
+		forward_path = "user-auth-form";
 		return forward_path;
 	}
 	
 	
-	@PostMapping("/user_auth_action")
+	@PostMapping("/user-auth-action")
 	public String user_auth_action(@ModelAttribute("fuser") User user, 
 			@RequestParam(value = "mailAuthKey", required = true) Integer mailAuthKey, Model model) throws Exception {
 	    int mailKey = (int) model.getAttribute("mailKey");
 	    model.addAttribute("mailKey", mailKey);
 	    if(mailKey == mailAuthKey){
 	        userService.mailAuthUpdate(user);
-	        return "user_main";
+	        return "user-main";
 	    }
-	    return "user_auth_form";
+	    return "user-auth-form";
 	}
 	
 	/*************************************/
@@ -119,81 +119,81 @@ public class UserController {
 	
 	@LoginCheck
 	@ApiOperation(value = "회원 정보 보기")
-	@GetMapping(value = "/user_view", produces = "application/json;charset=UTF-8")
+	@GetMapping(value = "/user-view", produces = "application/json;charset=UTF-8")
 	public String user_view(HttpServletRequest request) throws Exception {
 	    String forwardPath = "";
 	    HttpSession session = request.getSession();
 	    String sUserId = (String) session.getAttribute("sUserId");
 	    if (sUserId == null) {
 	        session.setAttribute("requestUrl", request.getRequestURL().toString());
-	        return "redirect:user_login_form";
+	        return "redirect:user-login-form";
 	    }
 	    User loginUser = userService.findUser(sUserId);
 	    request.setAttribute("loginUser", loginUser);
-	    forwardPath = "user_view";
+	    forwardPath = "user-view";
 	    
 	    return forwardPath;
 	}
 	
 	
 	@LoginCheck
-	@PostMapping("/user_modify_form")
+	@PostMapping("/user-modify-form")
 	public String user_modify_form(HttpServletRequest request) throws Exception {
 		String forwardPath = "";
 		String sUserId=(String)request.getSession().getAttribute("sUserId");
 		User loginUser=userService.findUser(sUserId);
 		request.setAttribute("loginUser", loginUser);
-		forwardPath="user_modify_form";
+		forwardPath="user-modify-form";
 		
 		return forwardPath;
 	}
 	
 	
 	@LoginCheck
-	@PostMapping("user_modify_action")
+	@PostMapping("user-modify-action")
 	public String user_modify_action(@ModelAttribute User user,HttpServletRequest request) throws Exception {
 		String forwardPath = "";
 		userService.update(user);
-		forwardPath="redirect:user_view";
+		forwardPath="redirect:user-view";
 		
 		return forwardPath;
 	}
 	
 	
 	@LoginCheck
-	@PostMapping("user_remove_action")
+	@PostMapping("user-remove-action")
 	public String user_remove_action(HttpServletRequest request) throws Exception {
 		String forwardPath = "";
 		String sUserId=(String)request.getSession().getAttribute("sUserId");
 		userService.remove(sUserId);
 		request.getSession().invalidate();
 		//forwardPath="forward:user_logout_action";
-		forwardPath="redirect:user_main";
+		forwardPath="redirect:user-main";
 		
 		return forwardPath;
 	}
 	
 	
 	@LoginCheck
-	@RequestMapping("user_logout_action")
+	@RequestMapping("user-logout-action")
 	public String user_logout_action(HttpServletRequest request) {
 		String forwardPath = "";
 		request.getSession(false).invalidate();
-		forwardPath="redirect:user_main";
+		forwardPath="redirect:user-main";
 		return forwardPath;
 	}
 	
 	
 	/***********GET방식요청시 guest_main redirection*********/
 	@GetMapping({
-				"user_write_action",
-				"user_login_action",
-				"user_modify_form",
-				"user_modify_action",
-				"user_remove_action"
+				"user-write-action",
+				"user-login-action",
+				"user-modify-form",
+				"user-modify-action",
+				"user-remove-action"
 				})
 	public String user_get() {
-		String forwardPath = "redirect:user_main";
+		String forwardPath = "redirect:user-main";
 		return forwardPath;
 	}
 	
@@ -201,7 +201,7 @@ public class UserController {
 	/****************Local Exception Handler***********/
 	@ExceptionHandler(Exception.class)
 	public String user_exception_handler(Exception e) {
-		return "user_error";
+		return "user-error";
 	}
 
 }

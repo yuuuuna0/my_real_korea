5c32872debe8c26d2f88c0c245d6ea8448ab82f2
minyoung <ssssoooop@gmail.com>
2023-04-07 16:01:42 +0900

    freeboard insert push

--- 산출물/SQL/insert/freeboardcomment_insert.sql
+++ 산출물/SQL/insert/freeboardcomment_insert.sql
@@ -1,5 +1,15 @@
 --insert
-insert into free_board_comment values (FREE_BOARD_COMMENT_F_CO_NO_SEQ.nextval,'명동교자 칼국수 맛있어요',sysdate,1,'user3');
-insert into free_board_comment values (FREE_BOARD_COMMENT_F_CO_NO_SEQ.nextval,'카페는 어떠신가요.',sysdate,2,'user1');
-insert into free_board_comment values (FREE_BOARD_COMMENT_F_CO_NO_SEQ.nextval,'강원 맛집 저도 궁금하군요.',sysdate,3,'user2');
+insert into free_board_comment values (FREE_BOARD_COMMENT_F_CO_NO_SEQ.nextval,'여의도 더현대서울 지하 1층 삼성혈해물탕, 호우섬 추천 드려요. ',sysdate,1,'user3');
+insert into free_board_comment values (FREE_BOARD_COMMENT_F_CO_NO_SEQ.nextval,'연평도에서 잡은 싱싱한 게로 만든 백년게장 추천드려요. 경기도 화성입니다.',sysdate,2,'user1');
+insert into free_board_comment values (FREE_BOARD_COMMENT_F_CO_NO_SEQ.nextval,'면류 좋아하시면 삼척시 장호감자탕&문어막국수 추천해요.',sysdate,3,'user2');
+insert into free_board_comment values (FREE_BOARD_COMMENT_F_CO_NO_SEQ.nextval,'엘시티에 있는 연화리해물천국에서 와인과 저녁 식사 한 끼 추천합니다. ',sysdate,1,'user3');
+insert into free_board_comment values (FREE_BOARD_COMMENT_F_CO_NO_SEQ.nextval,'연평도에서 잡은 싱싱한 게로 만든 백년게장 추천 드려요. 경기도 화성입니다.',sysdate,2,'user1');
+insert into free_board_comment values (FREE_BOARD_COMMENT_F_CO_NO_SEQ.nextval,'면류 좋아하시면 삼척시 장호감자탕&문어막국수 추천해요.',sysdate,3,'user2');
+insert into free_board_comment values (FREE_BOARD_COMMENT_F_CO_NO_SEQ.nextval,'어린이와 함께라면  아쿠아리움 관광, 과학관 관람을 추천해요. 티켓예약 플랫폼을 통해 할인된 가격으로 입장하실 수 있어요.',sysdate,1,'user3');
+insert into free_board_comment values (FREE_BOARD_COMMENT_F_CO_NO_SEQ.nextval,'인천 지역 추천해요. 드라이브 가려면 영종도에 가보세요.',sysdate,3,'user2');
+insert into free_board_comment values (FREE_BOARD_COMMENT_F_CO_NO_SEQ.nextval,'벚꽃 많은 곳은 춘천 의암 호수 추천해요.',sysdate,1,'user3');
+insert into free_board_comment values (FREE_BOARD_COMMENT_F_CO_NO_SEQ.nextval,'부산 진구 송정3대국밥 추천합니다.',sysdate,2,'user1');
+insert into free_board_comment values (FREE_BOARD_COMMENT_F_CO_NO_SEQ.nextval,'다양한 레저 시설이 있어요. 대전역에서는 20분 걸려요.',sysdate,1,'user3');
+
+
 

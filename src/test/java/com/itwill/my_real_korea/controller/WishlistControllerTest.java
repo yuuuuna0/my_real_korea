f92408b7dbf378c96b453b5ed18206d341d8f078
minseonallykim <minsun947@gmail.com>
2023-04-12 11:11:55 +0900

    wishlistcontroller img 출력 확인

--- src/test/java/com/itwill/my_real_korea/controller/WishlistControllerTest.java
+++ src/test/java/com/itwill/my_real_korea/controller/WishlistControllerTest.java
@@ -11,6 +11,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
@@ -22,7 +23,9 @@ import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.itwill.my_real_korea.dto.ticket.Ticket;
+import com.itwill.my_real_korea.dto.ticket.TicketImg;
 import com.itwill.my_real_korea.dto.tour.Tour;
+import com.itwill.my_real_korea.dto.tour.TourImg;
 import com.itwill.my_real_korea.dto.wishlist.Wishlist;
 import com.itwill.my_real_korea.service.wishlist.WishlistService;
 
@@ -50,6 +53,13 @@ class WishlistControllerTest {
 		wishlistList.add(new Wishlist(1, sUserId, new Ticket(1, sUserId, null, 0, sUserId, sUserId, 0, null), null));
 		wishlistList.add(new Wishlist(2, sUserId, null, new Tour(2, sUserId, 0, 0, 0, sUserId, 0, sUserId, sUserId, 0, null)));
 		wishlistList.add(new Wishlist(3, sUserId, new Ticket(3, sUserId, null, 0, sUserId, sUserId, 0, null), null));
+		for (Wishlist wishlist : wishlistList) {
+			List<TourImg> tourImgList = Arrays.asList(new TourImg(0, "dd", 2), new TourImg(0, "ff", 2));
+			List<TicketImg> ticketImgList = Arrays.asList(new TicketImg(0, "gg", 1), new TicketImg(0, "ggg", 1), 
+														new TicketImg(0, "ee", 3), new TicketImg(0, "vv", 3));
+			wishlist.setTourImgList(tourImgList);
+			wishlist.setTicketImgList(ticketImgList);
+		}
 		
 		// selectAllWithTicketAndTour() 메소드 실행시 wishlistList 가 return 된다고 가정
 		given(wishlistService.selectAllWithTicketAndTour(sUserId)).willReturn(wishlistList);
@@ -57,6 +67,8 @@ class WishlistControllerTest {
 		.andExpect(status().isOk()) // HTTP 상태코드가 200 OK
 		.andExpect(model().attributeExists("wishlistList")) // model 에 setAttribute 한 값이 있는지
 		.andExpect(model().attributeExists("sUserId")) // model 에 setAttribute 한 값이 있는지
+		.andExpect(model().attributeExists("tourImgList")) // model 에 setAttribute 한 값이 있는지
+		.andExpect(model().attributeExists("ticketImgList")) // model 에 setAttribute 한 값이 있는지
 		.andExpect(view().name("wishlist")) // wishlist 뷰를 반환하는지
 		.andDo(print());  // 콘솔에 요청과 응답을 출력
 		

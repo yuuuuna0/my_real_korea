2ec4515b06d3fbf6ad3da96b29caefda92984fcc
minyoung <ssssoooop@gmail.com>
2023-04-12 18:25:35 +0900

    freeboard 수정중

--- src/test/java/com/itwill/my_real_korea/service/freeboard/FreeBoardServiceImplTest.java
+++ src/test/java/com/itwill/my_real_korea/service/freeboard/FreeBoardServiceImplTest.java
@@ -25,7 +25,8 @@ class FreeBoardServiceImplTest {
                 , "어디로가야하나요", null
                 , 0
                 , new City(3, "강원", 3, 3)
-                , "user1");
+                , "user1"
+                ,0);
         int insert = freeBoardService.insertBoard(freeBoard);
         assertThat(insert).isEqualTo(1);
     }
@@ -33,9 +34,9 @@ class FreeBoardServiceImplTest {
     @Test
     void update() throws Exception {
         FreeBoard updateFreeBoard = new FreeBoard(
-                3, "3수정테스트"
+                3, "3수정테스트" 
                 , "유럽여행 어디로 갈까요", null
-                , 0, new City(1, "서울", 10, 10), "user2");
+                , 0, new City(1, "서울", 10, 10), "user2",0);
         freeBoardService.updateFreeBoard(updateFreeBoard);
     }
     @Disabled
@@ -69,7 +70,7 @@ class FreeBoardServiceImplTest {
         FreeBoard freeBoard = new FreeBoard(
                 2, "12345678901234567"
                 , "유럽여행 어디로 갈까요", null
-                , 0, new City(1, "seoul", 13, 13), "id2");
+                , 0, new City(1, "seoul", 13, 13), "id2",0);
         String titleString = freeBoardService.getTitleString(freeBoard);
         System.out.println("titleString = " + titleString);
     }

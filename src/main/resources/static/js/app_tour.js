cf4f9da3824a169044d38b030f0facf0a46867f1
jeong-yoona <jeong-yoona@localhost>
2023-04-13 01:00:04 +0900

    tour  필터 정렬 ajax 완료 (toType, pMethod 테이블 생성 예정)

--- src/main/resources/static/js/app_tour.js
+++ src/main/resources/static/js/app_tour.js
@@ -8,7 +8,7 @@ import * as Request from "./request.js";
 // JSON.stringify() => 객체를 string 으로, JSON.parse() => string 을 객체로 만듦 
 
 
-
+/*
 //1. 투어 검색하기
 $(document).on('click','#tour-search-btn',function(e){
 	let keyword=$('#tour-search-keyword').val();
@@ -29,25 +29,37 @@ $(document).on('click','#tour-search-btn',function(e){
 						},async);
 	e.preventDefault();	//이벤트의 기본동작 취소 -> 이벤트버블링,캡쳐링 중단 / 기본동작 취소 (ex.rendering
 });
+*/
 //2. 정렬
-$(document).on('change','#sort-by',aaa);
-$(document).on('click',".city_type,.iCheck-helper",function(e){
-	console.log(e.target);
+$(document).on('change',$('#sort-by,#city-checkbox,#toType-chceckbox'),aaa);
+//$(document).on('change',"#city-checkbox",aaa);
+$(document).on('click',"#tour-search-btn",aaa);
+
+//3. 체크박스 하나만 선택하기
+$('input[type="checkbox"][name="city-checkbox"]').click(function(){
+	if($(this).prop('checked')){
+		$('input[type="checkbox"]').prop('checked',false);
+		$(this).prop('checked',true);
+	}
 });
+
 
 function aaa(){
 	
 	let keyword=$('#tour-search-keyword').val();
 	let cityNo;
 	let toType;
-	let sortOrder=$(this).val();
-	if($('#cityNo').prop('checked')){
-		cityNo=$('#cityNo').prop('checked').val();
+	let sortOrder=$('#sort-by').val();
+	console.log($('#city-checkbox:checked').length);
+	console.log($('#toType-checkbox:checked').length);
+	
+	if($('#city-checkbox:checked').length==1){
+		cityNo=$('#city-checkbox:checked').val();
 	} else{
 		cityNo=0;
 	}
-	if($('#toType').prop('checked')){
-		toType=$('#toType').prop('checked').val();
+	if($('#toType-checkbox:checked').length==1){
+		toType=$('#toType-checkbox:checked').val();
 	} else{
 		toType=0;
 	}

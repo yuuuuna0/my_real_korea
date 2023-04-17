34d370e609b1be480ff50e2ee1671ee5ec77455a
kgee2 <kgee12300@gmail.com>
2023-04-17 18:08:48 +0900

    ..

--- src/main/resources/static/js/app_user.js
+++ src/main/resources/static/js/app_user.js
@@ -5,6 +5,90 @@ import * as Request from "./request.js";
 
 	
 //이메일 미인증 상태면 인증 폼 호출
+/*
+$(document).ready(function() {
+  // user-auth-template 템플릿을 렌더링하는 함수
+  function renderUserAuthTemplate() {
+    let template = $('#user-auth-template').html();
+    let renderTemplate = Handlebars.compile(template);
+    $('#login').html(renderTemplate());
+  }
+
+  //로그인 버튼을 클릭하면 authUser.getMailAuth() != 1 일 때만 user-auth-template을 렌더링
+  $('#login_btn').click(function(e) {
+    e.preventDefault();
+    
+      // 바로 로그인 실행되게 수정
+      $.ajax({
+        type: 'POST',
+        url: 'user-login',
+        contentType: 'application/json;charset=UTF-8',
+        data: JSON.stringify({
+          userId: $('#user_id').val(),
+          password: $('#password').val(),
+        }),
+        success: function(data) {
+			if (data.authUser != 1) {
+			console.log(data.authUser);
+		      renderUserAuthTemplate();
+		      /*
+	      }else{
+			
+		}
+		      
+          if (data.status == 0) {
+            window.location.href = data.data;
+          } else {
+            alert(data.message);
+          }
+        },
+        error: function(xhr, status, error) {
+          alert(error);
+        },
+      });
+    }
+  );
+});
+*/
+/*
+$(document).ready(function() {
+  // user-auth-template 템플릿을 렌더링하는 함수
+  function renderUserAuthTemplate() {
+    let template = $('#user-auth-template').html();
+    let renderTemplate = Handlebars.compile(template);
+    $('#login').html(renderTemplate());
+  }
+
+  //로그인 버튼을 클릭하면 authUser.getMailAuth() != 1 일 때만 user-auth-template을 렌더링
+  $('#login_btn').click(function(e) {
+    e.preventDefault();
+    
+      // 바로 로그인 실행되게 수정
+      $.ajax({
+        type: 'POST',
+        url: 'user-login',
+        contentType: 'application/json;charset=UTF-8',
+        data: JSON.stringify({
+          userId: $('#user_id').val(),
+          password: $('#password').val(),
+        }),
+        success: function(data) {
+			if (data.status === 1) {
+			  console.log(data.message);
+			  renderUserAuthTemplate();
+			} else {
+			  console.log(data.message);
+			}
+        },
+        error: function(xhr, status, error) {
+          alert(error);
+        },
+      });
+    }
+  );
+});
+*/
+
 $(document).ready(function() {
   // user-auth-template 템플릿을 렌더링하는 함수
   function renderUserAuthTemplate() {
@@ -19,6 +103,7 @@ $(document).ready(function() {
     renderUserAuthTemplate();
   });
 });
+
 
 
 //아이디, 비밀번호 찾기 폼

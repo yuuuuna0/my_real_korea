35a4af437dc9ba0fd706cb77cbb9c3c0da980048
kgee2 <kgee12300@gmail.com>
2023-04-19 10:28:58 +0900

    FileUploadConfig 생성 후 bean 등록, FileUploadApplication 삭제

---
+++ src/main/java/com/itwill/my_real_korea/config/FileUploadConfig.java
@@ -0,0 +1,23 @@
+package com.itwill.my_real_korea.config;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.itwill.my_real_korea.util.FileUploadProperties;
+import com.itwill.my_real_korea.util.FileUploadService;
+
+@Configuration
+@EnableConfigurationProperties(FileUploadProperties.class)
+public class FileUploadConfig {
+
+	@Bean
+	CommandLineRunner init(FileUploadService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
+	}
+	
+}

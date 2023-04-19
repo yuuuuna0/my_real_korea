8b17e5723f7897411724465bf5b3a37ea0c73183
minyoung <ssssoooop@gmail.com>
2023-04-19 19:12:01 +0900

    freeboard List

--- src/main/resources/static/js/app_freeboard.js
+++ src/main/resources/static/js/app_freeboard.js
@@ -4,24 +4,23 @@ import * as Request from "./request.js";
 $(document).on('click','#freeboard-search-btn',function(e){
 	// ajax로 리스트 부분만 검색된 리스트로 변경
 	let keyword = $('#freeboard-search-keyword').val();
-	let url = `freeBoard-search?pageNo=${keyword}&keyword=${keyword}`;
+	let url = `freeBoard-search?pageNo=1&keyword=${keyword}`;
 	let method = 'GET';
 	let contentType = 'application/json;charset=UTF-8';
-
 	let sendData;
 	let async = true;
 	
 	// 키워드 결과, 현재 페이지 숫자 표시
-	let curPage = $('#cur-page');
-	let totRecordCount = $('#tot-record-count');
-	
+	let curPage=$('#cur-page');
+	let totRecordCount=$('#tot-record-count');
 	// JSON.stringify() => 객체를 string 으로, JSON.parse() => string 을 객체로 만듬
 	Request.ajaxRequest(url, method, contentType, 
 						JSON.stringify(sendData),
 						function(resultJson){
 							//페이지 정보 변경
 							curPage.text(resultJson.data.pageMaker.curPage);
-							totRecordCount.text(resultJson.data.totRecordCount);
+							totRecordCount.text(resultJson.data.pageMaker.totCount);
+							
 							//code 1 일때 render, 아닐 때 msg 띄움
 							if(resultJson.code == 1){
 								View.render("#freeboard-search-list-template", resultJson, '#freeboard-list');
@@ -39,14 +38,11 @@ $(document).on('change','#sort-by',function(e){
 	console.log(selectedValue);
 	if(selectedValue === "freeboard-list-fBoNo-desc"){
 		// 최신순
-		let url = 'freeboard-list-fBoNo-desc';
+		let url = 'fBoList';
 		let method = 'GET';
 		let contentType = 'application/json;charset=UTF-8';
-		let sendData = {};
 		let async = true;
-	
 		Request.ajaxRequest(url, method, contentType, 
-							sendData,
 							function(resultJson){
 								//code 1 일때 render, 아닐 때 msg 띄움
 								if(resultJson.code == 1){
@@ -60,14 +56,12 @@ $(document).on('change','#sort-by',function(e){
 		
 	if(selectedValue === "freeboard-list-fBoNo-asc"){
 		// 오래된순
-		let url = 'freeboard-list-fBoNo-asc';
+		let url = 'fBoList-fBoNo-asc';
 		let method = 'GET';
 		let contentType = 'application/json;charset=UTF-8';
-		let sendData = {};
 		let async = true;
 	
 		Request.ajaxRequest(url, method, contentType, 
-							sendData,
 							function(resultJson){
 								//code 1 일때 render, 아닐 때 msg 띄움
 								if(resultJson.code == 1){
@@ -79,16 +73,14 @@ $(document).on('change','#sort-by',function(e){
 		e.preventDefault();
 		}
 		
-	if(selectedValue === "reeboard-list-readCount-desc"){
+	if(selectedValue === "freeboard-list-readCount-desc"){
 		// 조회수 높은순
-		let url = 'reeboard-list-readCount-desc';
+		let url = 'fBolist-readcount-desc';
 		let method = 'GET';
 		let contentType = 'application/json;charset=UTF-8';
-		let sendData = {};
 		let async = true;
 	
 		Request.ajaxRequest(url, method, contentType, 
-							sendData,
 							function(resultJson){
 								//code 1 일때 render, 아닐 때 msg 띄움
 								if(resultJson.code == 1){

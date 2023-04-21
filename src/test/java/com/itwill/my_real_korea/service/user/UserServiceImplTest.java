9e9a40f7350a7f56eec7dd590edf90932864d9d8
kgee2 <kgee12300@gmail.com>
2023-04-21 21:29:55 +0900

    admin

--- src/test/java/com/itwill/my_real_korea/service/user/UserServiceImplTest.java
+++ src/test/java/com/itwill/my_real_korea/service/user/UserServiceImplTest.java
@@ -40,11 +40,8 @@ class UserServiceImplTest {
 	//4. 회원 정보 수정
 	@Test
 	void testUpdate() throws Exception {
-		User user = userService.findUser("swagger3");
-		user.setPassword("asdfasdf1!");
-		user.setNickname("swagger3");
-		user.setPhone("011-1111-1111");
-		user.setEmail("kgee12300@gmail.com");
+		User user = userService.findUser("admin");
+		user.setPassword("admin0000");
 		System.out.println(">> updated :"+userService.update(user));
 	}
 	

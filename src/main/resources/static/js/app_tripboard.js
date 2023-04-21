4f03ed28eaddec830875f91d6933d07d8be7bafb
seunggeunYoo <yooyoosg@naver.com>
2023-04-21 10:08:59 +0900

    tripboard-detail 수정중
    dao,service 에 method 추가.
    
    동행게시판 댓글 삭제 성공.

--- src/main/resources/static/js/app_tripboard.js
+++ src/main/resources/static/js/app_tripboard.js
@@ -187,3 +187,98 @@ $(document).on('click', '#tripboard-modify-action', function(e){
 	e.preventDefault();
 });
 
+//동행게시판 댓글 삭제
+$(document).on('click', '#comment_btn_delete', function(event) {
+    event.preventDefault();
+    let tCoNo = $(this).data("value");
+    let tBoNo = $('#tBoNo-hidden-value').val();
+    
+    console.log(tCoNo);
+    console.log(tBoNo);
+
+    $.ajax({
+        type: 'DELETE',
+        url: 'tripboardcommentdeleteaction/' + tCoNo,
+        success: function(result) {
+            // 삭제 후 tripboard-detail로 상태유지
+            window.location.href = 'tripboard-detail?tBoNo=' + tBoNo;
+        },
+        error: function(xhr, status, error) {
+            console.log(error);
+        }
+    });
+});
+
+
+//동행게시판 댓글 수정 폼
+$(document).on('click', '#tripboardcomment-modify-form-btn', function(e) {
+ 
+    let tCoNo = $(this).attr("data-value");
+    let url = 'tripboardcomment/' + tCoNo;
+    let method = 'GET';
+    let contentType = 'application/json;charset=UTF-8';
+    console.log(tCoNo);
+
+    let sendData = {};
+    let async = true;
+   
+    
+    Request.ajaxRequest(url, method, contentType, sendData,
+      
+        function(resultJson) {
+          
+            if (resultJson.code == 1) {
+                View.render("#tripBoardCommentModifyForm", resultJson, '#tripboardcomment-detail');
+            } else {
+                alert(resultJson.msg);
+            }
+            ;
+        }, async);
+
+    e.preventDefault();
+});
+
+// 동행게시판 댓글 폼 액션
+$(document).on('click','#tripboardcomment-modify-action-btn',function(e){
+  
+    let tCoNo = $(this).data("value");
+    let tBoNo = $('#tBoNo-hidden-value2').val();
+    let tcommentContent = $('textarea[name="tCoContent2"]').val();
+    let userId = $("h4[name='tCoUserId2']").text();
+    let url = 'tripboardcommentupdateaction/'+tCoNo;
+    let method = 'PUT';
+    let contentType = 'application/json;charset=UTF-8';
+    console.log('tCoNo'+tCoNo);
+    console.log('tcommentContent'+tcommentContent);
+    console.log('tBoNo'+tBoNo);
+    console.log('tCoDate'+$('#tCoDate2').val());
+    console.log('userId'+userId);
+
+    let sendData= {
+        tcoNo: tCoNo,
+        tboNo:$('#tBoNo-hidden-value2').val(),
+        tcommentContent:conetent,
+        tcommentDate:$('#tCoDate2').val(),
+        userId: userId
+    };
+    let async = true;
+    /*
+    // JSON.stringify() => 객체를 string 으로, JSON.parse() => string 을 객체로 만듬
+    Request.ajaxRequest(url, method, contentType,
+        JSON.stringify(sendData),
+        function(resultJson){
+            //code 1 일때 render, 아닐 때 msg 띄움
+            if(resultJson.code == 1){
+                //window.location.href = 'tripboard-detail?tBoNo='+tBoNo;
+                 View.render("#tripBoardCommentModifyAction", resultJson, '#tripboardcomment-detail');
+            } else {
+                alert(resultJson.msg);
+            };
+        }, async);
+*/
+    e.preventDefault();
+});
+
+
+
+

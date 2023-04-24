510b2931745dc04705b9491a2c09244c0661533e
minyoung <ssssoooop@gmail.com>
2023-04-24 18:30:20 +0900

    freeboard sql

--- 산출물/SQL/insert/freeborad_insert.sql
+++ 산출물/SQL/insert/freeborad_insert.sql
@@ -1,59 +1,23 @@
 --insert
-insert into free_board values (free_board_f_bo_no_seq.nextval,'서울 여행가는데 추천좀 해주세요',
-                               '1박2일로 가는데 첫째날은 롯데월드가고 잠실쪽에 숙소 잡아놨는데 센트럴로 출발해야되서 지하철을 타도 오래걸리지 않는 지역주변으로 볼만한게 있을까요
-                                관심사가 헤어 인테리어 자동차라 박람회나 전시회장도 괜찮습니다','2023-01-11',10,1,'aaa111');
-insert into free_board values (free_board_f_bo_no_seq.nextval,'제주도 3박 4일 여행',
-                               '이번에 여자친구랑 제주도 3박4일 여행 가려고 하는데 여행코스랑 숙소 음식점 추천 부탁드립니다
-                               4.21~4.24일 3박 4일 여행으로 21일에 오후 4시 도착 24일에는 오후 2시 출발입니다.
-                               여행은 중문(서귀포)에서 애월 공항 이런 순으로 여행코스 짜고 싶습니다.'
-                               ,'2023-01-19',3,7,'myrealkorea1');
-insert into free_board values (free_board_f_bo_no_seq.nextval,'제주도 여행 비용 문의',
-                                '제주도 여행 비용이 어떻게 될까요?
-                                가족 여행으로 5월 경 제주도 여행을 예정 중입니다.
-                                어르신들이 있어 김포공항에서 출발하는 조건으로 패키지 여행 경비가 어느정도 될까요?
-                                인원은 6인(성인)입니다.','2023-01-22',0,7,'myrealkorea2');
-insert into free_board values (free_board_f_bo_no_seq.nextval,'질문2박3일 아이와 부산여행',
-                                '이번주 급 부산여행을 계획하고 있는데
-                                서울에서 부산내려가서 아이와 가볼만한 여행코스나
-                                가기전 들릴만한 명소나
-                                혹은 다른코스라도 여행추천지 알려주세요~^^~','2022-01-19',7,4,'bbb111');
-insert into free_board values (free_board_f_bo_no_seq.nextval,'경기도 여행에 대한 질문입니다!',
-                               '안녕하세요! 이제 2023년도 20살 되는 학생 겸 청년입니다. 저희가 졸업하고 친구들과 여행 준비하고 있습니다.
-                                차마 저희가 운전을 잘 하지도 못하고 안전하지도 않아서 대중교통을 이용해서 여행을 가려고 합니다.
-                                하지만 찾아봐도 좋은 여행지가 보이지 않아 걱정이 됩니다. 2박 3일로 놀러갈 예정인데 좋은 경기도 여행지 추천 해주시면 감사하겠습니다.',sysdate,20,2,'aaa111');
-insert into free_board values (free_board_f_bo_no_seq.nextval,'대전 여행....도와주세요',
-                               '언니랑 둘이 대전여행 가는데 이제 중1되는 애랑 이제 고1되는 애 둘이서 들어갈수 있는 "따뜻한" 물이 있고 가성비 좋은 온천이나 수영장 있을까요? 아 참고로 호텔이나 숙박업소 아닌곳으로 알려주셨으면 좋겠어요 ',
-                               '2023-02-12',1,5,'user2');
-insert into free_board values (free_board_f_bo_no_seq.nextval,'부모님과 1박2일 여행 (전주)','5월 29.30일에 부모님과 1박2일 전주 여행을 하려고 합니다. ' ||
-                                                                                  '코스 추천좀 부탁드리겠습니다.','2023-03-19',9,6,'user11');
-
-
-insert into free_board values (free_board_f_bo_no_seq.nextval,'겨울 강원도 여행 3박4일 문의합니다','1월말쯤 통영에서 자차로 강원도 여행을 생각중입니다.
-숙소는 두군데로 생각중입니다
-1. 속초 온천
-2. 스카이베이 경포
-
-속초랑 경포(강릉쪽이죠?) 어느쪽을 2박으로 잡는게 나을까요?
-- 맛집이 더 많은 곳은 어디인가요?
-(대게..?홍게..? 무튼 이런거 먹고싶어요)
-- 신혼부부가 여행하며 구경하거나 볼거리 더 많은 곳은 어디 근처인가요?
-(경포쪽을 2박으로 생각했었는데 그 이유가 정동진 근처도 왕복 1시간이면 갔다올수 있어서 2박하면서 하슬라아트월드 갔다가 다시 숙소로 돌아와도 되겠다 싶었는데 그건 1박으로도 가능할것같아서 고민중입니다ㅠㅠ)
-
-위의 두 조건의 만족이 더 많은곳으로 2박을 하고싶어서요~
-
-제가 시간 여유가 없어 광고글은 신고들어갈꺼구요 정말 현지인분들이나 여행 다녀오셔서 추천하고싶다 하시는 분들 답변 부탁드립니다!!!!','2022-12-03',13,3,'test11');
-insert into free_board values (free_board_f_bo_no_seq.nextval,'전주여행 갈곳, 전시회 추천',
-                               '제가 이번달 말에 남자친구랑 전주여행을 가는데요. 전주한옥마을만 가기엔 그래서 갈만한 곳, 공방, 전시회같은거 있음 추천부탁드려요ㅠㅠ','2022-07-03',3,6,'qqqq1111');
-insert into free_board values (free_board_f_bo_no_seq.nextval,'	전주 여행코스 추천이요~!',
-                               '영화제 기간 일정으로 2박3일 전주여행 좀 가볼까 해서요.
-                                여행코스 추천 좀 해주세요
-                                어차피 주목적은 전주국제영화제니까 많은 곳을 둘러보지는 못할거에요
-                                그냥 전주하면 꼭 가볼만한 곳 알짜배기만 추천해주세욜~~!!
-                                전주 영화의 거리를 기점으로 최대한 이동이 편했으면 좋겠어요. :-)','2022-09-03',22,6,'qqqq1111');
-
-
-
-
-
-
-
+insert into free_board values (free_board_f_bo_no_seq.nextval,'서울 맛집 추천','위생깔끔한곳 선호합니다','2023-01-11',0,1,'user1');
+insert into free_board values (free_board_f_bo_no_seq.nextval,'경기 맛집 추천','클린한 음식으로 추천부탁합니다','2023-01-19',3,2,'user2');
+insert into free_board values (free_board_f_bo_no_seq.nextval,'강원 맛집 추천','면류 좋아합니다','2023-01-22',0,3,'user3');
+insert into free_board values (free_board_f_bo_no_seq.nextval,'부산 해운대 1박2일 추천','자연보는거 좋아합니다','2022-01-19',7,4,'user1');
+insert into free_board values (free_board_f_bo_no_seq.nextval,'대전 여행코스 추천해주세요','2박 3일 코스로 여행 예정입니다','2023-02-12',1,5,'user2');
+insert into free_board values (free_board_f_bo_no_seq.nextval,'서울 핫플 추천받습니다','사람많은곳 선호합니다','2023-03-19',9,1,'user3');
+insert into free_board values (free_board_f_bo_no_seq.nextval,'경기도 풍경 좋은곳 추천해주세요','1박2일 여행가려고요','2022-01-03',6,2,'user1');
+insert into free_board values (free_board_f_bo_no_seq.nextval,'경기도 파주쪽 어죽맛집 추천해주세요','파주여행갑니다','2022-07-03',6,2,'user1');
+insert into free_board values (free_board_f_bo_no_seq.nextval,'강원도 벚꽃축제 하나요?','벚꽃좀 보고 싶네요','2022-09-03',13,3,'user2');
+insert into free_board values (free_board_f_bo_no_seq.nextval,'부산 국밥집 어디가 잘하나요','근본있는곳으로 추천부탁합니다','2022-11-03',11,4,'user3');
+insert into free_board values (free_board_f_bo_no_seq.nextval,'대전 유성온천 어떤가요','가보신분 후기좀 남겨주세요',sysdate,4,5,'user1');
+insert into free_board values (free_board_f_bo_no_seq.nextval,'서울 맛집리스트 부탁해요','점심 때 불고기 백반 먹고싶어요.','2023-04-11',1,1,'user1');
+insert into free_board values (free_board_f_bo_no_seq.nextval,'제주 고기국수 추천','제주 고기국수 유명한 집이요.','2023-01-18',1,6,'user3');
+insert into free_board values (free_board_f_bo_no_seq.nextval,'서울말고 근교에서 괜찮은 고깃집 있나요.','웨이팅 있어도 괜찮아요. 고기면 다 좋습니다.','2023-01-20',1,2,'user1');
+insert into free_board values (free_board_f_bo_no_seq.nextval,'통영 여행 코스 추천','뚜벅이여행입니다.','2022-01-10',1,5,'minyoung1');
+insert into free_board values (free_board_f_bo_no_seq.nextval,'동백꽃 볼 수 있는 곳 추천이요','제주도 여행가서 시간 남으면 보러 가려고요','2023-02-25',1,6,'user3');
+insert into free_board values (free_board_f_bo_no_seq.nextval,'서울 인근 핫플 추천받습니다','개인적으로 곱창 선호합니다','2023-03-10',1,2,'user3');
+insert into free_board values (free_board_f_bo_no_seq.nextval,'서울에서 놀러 갈 수 있는 곳 추천','가족, 친구들과 여행 갈 때 갈 수 있는 곳으로 부탁해요.'2022-01-03',6,2,'user1');
+insert into free_board values (free_board_f_bo_no_seq.nextval,'국내 호텔 비수기와 성수기가 언제인지 궁금합니다.','국내 호텔 성수기, 비수기에 대해서 검색해봐도 말도 다 다르고 그마저도 거의 나오지 않아서 (해외 호텔이 아닌 국내 호텔의 비수기, 성수기가 궁금합니다)  정확한 날짜 (몇월인지 이왕이면 더 구체적으로)가 언제인지 궁금합니다. 언제인가요? 그리고 지금은 비수기인가요?','2022-07-03',1,0,'user1');
+insert into free_board values (free_board_f_bo_no_seq.nextval,'임산부 국내여행','전 여행은 좋아하는데 계획을 너무 못짜서 부탁드립니다 일정은 2박3일이나 3박4일로 생각중이예요',1,0,'user2');
+insert into free_board values (free_board_f_bo_no_seq.nextval,'혼자 갈 생각인데 (1인) 국내 일본 느낌 물씬 나는 료칸 (온천) 없을까요','지역 상관 없구요 너무 비싸지만 않았으면 좋겠는데 개인노천탕이나 방에 탕 있었으면 좋겠어요 (없어도 일단은 추천받습니다) 굳이 료칸이 아니더라도 뭐 일본식 느낌나는 호텔이나 숙박 업계 소개 부탁드립니다 ~!','2023-04-24',1,0,'user3');
+insert into free_board values (free_board_f_bo_no_seq.nextval,'국내 1박2일 여행갈만한곳 추천좀','참고로 강릉,속초,양양,태안,안동,서산은 다 가봐서 안가본곳쪽으로 경험자분들 추천.부탁드려요',sysdate,1,0,'user1');

6ad1e8856e22bf827c10099ac7ac85f7358d8d47
minseonallykim <minsun947@gmail.com>
2023-04-25 19:08:23 +0900

    채팅 완료 공지사항 이미지 출력 수정완료

--- src/main/resources/static/js/app_chat_tripboard.js
+++ src/main/resources/static/js/app_chat_tripboard.js
@@ -2,9 +2,21 @@ import * as View from "./view.js";
 import * as Request from "./request.js";
 
 
-/***************채팅방 팝업 열기******************/
+/***************동행게시판에서 채팅방 팝업 열기******************/
 $(document).on('click','#chat-pop-up-click', function(e) {
 	let receiverId = $('#chat-tripboard-userId').val();
+	console.log('????????? openPopup receiverId : '+receiverId);
+	let url = 'chat?receiverId='+receiverId;
+	
+	window.open(url,
+				'target',
+				'top=100, left=200, width=1000, height=800, toolbar=no, menubar=no, location=no, status=no, scrollbars=no, resizable=no');
+
+});
+
+/***************상단 아이콘으로 채팅방 팝업 열기******************/
+$(document).on('click','#chat-pop-up', function(e) {
+	let receiverId = $(this).attr('data-user-id');
 	console.log('????????? openPopup receiverId : '+receiverId);
 	let url = 'chat?receiverId='+receiverId;
 	

adc667e4d404fff62052fdf577ba83d9987415d7
daeun005 <zkdos12@gmail.com>
2023-04-26 14:21:36 +0900

    마이페이지

--- src/main/resources/static/js/view.js
+++ src/main/resources/static/js/view.js
@@ -23,12 +23,12 @@ export function render(templateId, jsonResult={}, contentId){
 	  return date.toLocaleDateString('ko-KR', { month: '2-digit', day: '2-digit' });
 	});
 	Handlebars.registerHelper('dateFormat3', function(dateString) {
- 	  var date = new Date(dateString);
-	  var year = date.getFullYear();
-	  var month = ('0' + (date.getMonth() + 1)).slice(-2);
-	  var day = ('0' + date.getDate()).slice(-2);
-	  return year + '년 ' + month + '월 ' + day + '일';
-});
+	 	  var date = new Date(dateString);
+		  var year = date.getFullYear();
+		  var month = ('0' + (date.getMonth() + 1)).slice(-2);
+		  var day = ('0' + date.getDate()).slice(-2);
+		  return year + '-' + month + '-' + day;
+	});
 
 	
 	//if함수 (if else 아님)

02dd4a97a3294cfa275ea2f1819fe21375ce8f4d
kgee2 <kgee12300@gmail.com>
2023-04-28 01:17:13 +0900

    회원가입 후 로그인 화면으로 전환

--- src/main/resources/static/js/app_user.js
+++ src/main/resources/static/js/app_user.js
@@ -600,11 +600,11 @@ $(document).on('click','#btn-user-create',function(e) {
 	    success: function (data) {
 	        if (data.status == 0) {
 //	            alert(data.message);
-//	            window.location.href = "./index";
 				Swal.fire({
 					icon: 'success',
 					text: data.message
 				})
+	            window.location.href = "./index";
 	        } else {
 //	            alert(data.message);
 				Swal.fire({
